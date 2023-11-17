c8f98dd1eb6d442622ffd992ae8885cea91f0667
aluno <Aluno@PR01-COMP2-26.CSED.LAB>
2023-11-17 09:00:32 -0300

    trab

--- js/arataki.js
+++ js/arataki.js
@@ -1,1 +1,16 @@
 alert("PÁGINA SOBRE ARATAKI");
+console.log
+var btnTerceiro = document.querySelector(".btnTerceiro");
+console.log(btnTerceiro);
+
+btnTerceiro.addEventListener("click", () => {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+
+  var corAleatoria = "rgb(" + red + "," + green + "," + blue + ")";
+
+  console.log(corAleatoria);
+
+  document.body.style.backgroundColor = corAleatoria;
+});
--- js/kazuha.js
+++ js/kazuha.js
@@ -1,6 +1,4 @@
 console.log("Arquivo index.js carregado com sucesso!");
-
-//var btnTerceiro = document.getElementsByClassName('btnTerceiro')[0];
 var btnTerceiro = document.querySelector(".btnTerceiro");
 console.log(btnTerceiro);
 
